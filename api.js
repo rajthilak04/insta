4f5426731e8723450e3da4609df6b6e5f7b485d7
Rajthilak <rajthilak04@gmail.com>
2018-03-29 11:58:18 +0530

    latest

--- api.js
+++ api.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+var fs = require('fs');
 
 router.post('/users', (req, res) => {
-    console.log(req.params);
-    console.log(req.body);
-    res.send('test');
+    // console.log(req.params);
+    // console.log(req.body);
+    // res.send('test');
+    var data = JSON.stringify(req.body);
+
+    fs.appendFile('demofile1.txt', data, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+        res.end();
+      });
+
    
  });
 
